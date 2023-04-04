4fe37a50a3c2590b812e92c39777113978ae1020
Elegard <whiteandy21321@gmail.com>
2023-04-04 20:39:47 +0300

    Add virst and second values

--- app.rb
+++ app.rb
@@ -1,12 +1,21 @@
 require'sinatra'
-require 'rubygems'
+require'rubygems'
 require'sinatra/reloader'
 
 get'/' do
-   erb:home
+      erb "Hello! enter the home tab and let's get started"
 end
 
 
+get'/home' do
+   erb:home
+end
 
-
+post'/home' do
+   @first=params[:a]
+   @second=params[:b]
+   #@symbol=params[:c]
+   #erb "вычисление значений"
+   erb:home
+end
 #error
