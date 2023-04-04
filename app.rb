e21a1a61c4ea34509ea621e9af1ff0fd1f87368e
Elegard <whiteandy21321@gmail.com>
2023-04-04 20:52:54 +0300

    Add @error function

--- app.rb
+++ app.rb
@@ -16,6 +16,17 @@ post'/home' do
    @second=params[:b]
    #@symbol=params[:c]
    #erb "вычисление значений"
+
+
+   hh={:a=>'Enter first value',:b=>'Enter second value'}
+
+   hh.each do |key,value| 
+      if params[key]==''
+         @error=hh[key]
+         erb:home
+      end
+   end
+
    erb:home
 end
 #error
