15b9a3aa40a57bf7c8f5ddf13f07bdc8b3dbd013
Elegard <whiteandy21321@gmail.com>
2023-04-05 17:19:10 +0300

    Add move functions

--- app.rb
+++ app.rb
@@ -14,19 +14,31 @@ end
 post'/home' do
    @first=params[:a]
    @second=params[:b]
-   @symbol=params[:c]
-   #erb "вычисление значений a+b"
+   @symbol=params[:choose]
 
-
-   hh={:a=>'Enter first value',:b=>'Enter second value',:c=>"Enter you move"}
+   hh={:a=>'Enter first value',:b=>'Enter second value',:choose=>"Enter you move"}
 
    hh.each do |key,value| 
       if params[key]==''
          @error=hh[key]
-         erb:home
+         return erb:home
       end
    end
 
-   erb:home
+   if @symbol=='+'
+      erb"#{@first.to_i + @second.to_i}" # что бы вычесление работало нужно писать за пределом цикла
+   elsif @symbol=='-'
+      erb"#{@first.to_i - @second.to_i}"
+   elsif @symbol=='*'
+      erb"#{@first.to_i * @second.to_i}"
+   elsif @symbol=='/'
+      erb"#{@first.to_i / @second.to_i}"
+   end
 end
-#error
+
+# В блоке post '/home' есть цикл, который проверяет, 
+# что все поля формы заполнены.
+#  Если какое-то поле не заполнено, то выводится сообщение об ошибке и возвращается шаблон home.
+#   Однако, если все поля заполнены, то код не выполняется, и результат сложения не выводится.
+#    Чтобы исправить это, нужно переместить строку erb "#{@first + @second}" за пределы цикла, чтобы она была вызвана,
+#  только если все поля заполнены.
